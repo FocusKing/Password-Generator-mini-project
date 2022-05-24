cd159f603d54229f10bce2c9d8511506edaba270
FocusKing <104179717+FocusKing@users.noreply.github.com>
2022-05-24 19:52:39 -0400

    added event Default

--- script.js
+++ script.js
@@ -7,46 +7,47 @@ function generatePassword() {
   var numbers = "0123456789";
   var special = "#$%&()*+,-./:;<=>?";
   var uppercase = lowercase.toUpperCase();
-}
-var input = parseInt(prompt("Choose a length between 8 and 128"));
-var passwordCharSet = "";
 
-if (input >= 8 && input <= 128) {
-  // GOOD CODE GOES HERE
-} else {
-  alert("NOT COOL");
+  var input = parseInt(prompt("Choose a length between 8 and 128"));
+  var passwordCharSet = "";
+
+  if (input >= 8 && input <= 128) {
+    // GOOD CODE GOES HERE
+  } else {
+    alert("NOT COOL");
+  }
+
+  var hasLowercase = window.confirm("Want to use lowercase letters?");
+  if (hasLowercase == true) {
+    passwordCharSet += lowercase;
+  };
+
+  var hasUppercase = window.confirm("Want to use uppercase letters?");
+  if (hasUppercase == true) {
+    passwordCharSet += uppercase;
+  };
+  var hasSymbols = window.confirm("Want to use symbols?");
+  if (hasSymbols == true) {
+    passwordCharSet += special;
+  };
+  var hasNumbers = window.confirm("Want to use numbers?");
+  if (hasNumbers == true) {
+    passwordCharSet += numbers;
+  };
+  console.log(passwordCharSet)
+  var result = "";
+  for (var i = 0; i < input; i++) {
+    result += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
+  }
+  return (result);
 }
 
-var lowercase = window.alert("Want to use lowercase letters?");
-if (lowercase == true) {
-  passwordCharSet += lowercase;
-};
-
-var uppercase = window.alert("Want to use uppercase letters?");
-if (uppercase == true) {
-  passwordCharSet += uppercase;
-};
-var symbols = window.alert("Want to use symbols?");
-if (symbols == true) {
-  passwordCharSet += symbols;
-};
-var numbers = window.alert("Want to use numbers?");
-if (numbers == true) {
-  passwordCharSet += numbers;
-};
-var password = "";
-for (var i = 0; i < length; i++) {
-  password += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
-}
-//return password;
-function myFunction() {
-  return "Access Allowed";
-}
 
 
 
 // Write password to the #password input
 function writePassword() {
+  event.preventDefault();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
