a624af5a2f7c6d1175dd06de82fd0b5bb78bc9fa
FocusKing <104179717+FocusKing@users.noreply.github.com>
2022-05-24 21:15:20 -0400

    NaN input added

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Instructor Provided: Anthony Cooper
+var storage = [];
+console.log(storage)
 function generatePassword() {
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var numbers = "0123456789";
   var special = "#$%&()*+,-./:;<=>?";
   var uppercase = lowercase.toUpperCase();
 
+
   var input = parseInt(prompt("Choose a length between 8 and 128"));
   var passwordCharSet = "";
 
-  if (input >= 8 && input <= 128) {
-    // GOOD CODE GOES HERE
+  if (!isNaN(input) && input >= 8 && input <= 128) {
+
+    var hasLowercase = window.confirm("Want to use lowercase letters?");
+    if (hasLowercase == true) {
+      passwordCharSet += lowercase;
+    };
+  
+    var hasUppercase = window.confirm("Want to use uppercase letters?");
+    if (hasUppercase == true) {
+      passwordCharSet += uppercase;
+    };
+    var hasSpecials = window.confirm("Want to use symbols?");
+    if (hasSpecials == true) {
+      passwordCharSet += special;
+    };
+    var hasNumbers = window.confirm("Want to use numbers?");
+    if (hasNumbers == true) {
+      passwordCharSet += numbers;
+    };
+    console.log(passwordCharSet)
+    var result = "";
+    for (var i = 0; i < input; i++) {
+      result += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
+    }
+    return (result);
+  
   } else {
     alert("TRY AGAIN");
+    return generatePassword();
   }
-
-  var hasLowercase = window.confirm("Want to use lowercase letters?");
-  if (hasLowercase == true) {
-    passwordCharSet += lowercase;
-  };
-
-  var hasUppercase = window.confirm("Want to use uppercase letters?");
-  if (hasUppercase == true) {
-    passwordCharSet += uppercase;
-  };
-  var hasSymbols = window.confirm("Want to use symbols?");
-  if (hasSymbols == true) {
-    passwordCharSet += special;
-  };
-  var hasNumbers = window.confirm("Want to use numbers?");
-  if (hasNumbers == true) {
-    passwordCharSet += numbers;
-  };
-  console.log(passwordCharSet)
-  var result = "";
-  for (var i = 0; i < input; i++) {
-    result += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
-  }
-  return (result);
 }
+  
 
+ 
 
 
 
